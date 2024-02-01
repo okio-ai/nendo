e7a103c0bb598d6ea5740f7112aeae9670cc0964
faradox <felix@okio.ai>
2024-02-01 19:16:16 +0100

    fix ordering

--- src/nendo/library/sqlalchemy_library.py
+++ src/nendo/library/sqlalchemy_library.py
@@ -1320,7 +1320,7 @@ class SqlAlchemyNendoLibrary(schema.NendoLibraryPlugin):
             if order_by:
                 if order_by == "random":
                     query_local = query_local.order_by(func.random())
-                if order_by == "collection":
+                elif order_by == "collection":
                     query_local = (
                         query_local
                         .order_by(
