a65880070c9051cc9a510fd751fc9195317d795a
Aaron Abebe <aaron.abebe@gmail.com>
2024-02-14 15:50:15 +0100

    fix: remove last flac file change again, doesnt work

--- src/nendo/library/sqlalchemy_library.py
+++ src/nendo/library/sqlalchemy_library.py
@@ -193,9 +193,6 @@ class SqlAlchemyNendoLibrary(schema.NendoLibraryPlugin):
                             sr=None,
                             mono=False,
                         )
-                    elif file_path.endswith(".flac"):
-                        signal, sr = sf.read(file=file_path, dtype="int32")
-                        signal = signal.astype(np.float64)
                     else:
                         signal, sr = sf.read(file=file_path)
                     # resample to default rate if required
